594b6577953dc27ec7edc82037ccf11d4a606033
Rebecca <rebeccads.95@hotmail.com>
2022-12-02 19:42:23 +0000

    Changes made

--- app.js
+++ app.js
@@ -8,38 +8,33 @@ function flip(){
   return rand;
 }
 
-
 function userChoice(){
   const outcome = flip();
   if (outcome === 1){
     document.querySelector("#outcome").textContent = "Heads"  
-    outcome_span.style.color = "red"
     outcome_span.style.transition = "0.3s"
+    const list = document.querySelector("ul")
+    const newItem = document.createElement("li")
+    newItem.textContent = "Heads";
+    list.appendChild(newItem)
   }
    if (outcome === 2){
     document.querySelector("#outcome").textContent = "Tails"
-    outcome_span.style.color = "green"
     outcome_span.style.transition = "0.3s"
+    const list = document.querySelector("ul")
+    const newItem = document.createElement("li")
+    newItem.textContent = "Tails";
+    list.appendChild(newItem)
   }
   
   setTimeout(function(){
     document.querySelector("#outcome").textContent = '';
 }, 1500);
-    addPrev();
-} //transparent only when timeout, need for original border colors to return when buttons are clicked again
+}; //transparent only when timeout, need for original border colors to return when buttons are clicked again
     
-
 
 
 heads.addEventListener("click", userChoice)
 tails.addEventListener("click", userChoice)
-
-function addPrev(){
-    let prev = document.querySelector("#outcome").value
-    const list = document.querySelector("ul")
-    const newItem = document.createElement("li")
-    newItem.textContent = prev;
-    list.appendChild(prev)
-};
 
 //figure a way to show the outcome has changed even if it is the same outcome
